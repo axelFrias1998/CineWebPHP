bfee03ee8bc7fac38faf6b32e694ddafef366603
Axel Frías Hernández <axel.frias257@gmail.com>
2019-05-25 03:27:16 -0500

    Modal

--- header.php
+++ header.php
@@ -14,29 +14,140 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
-            <a class="navbar-brand" href="#">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                    <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="navlink" href="#">Contact</a>
-                </li>
-                </ul>
-            </div>
+                <img src="img/Cinezone.jpg" width="10%" height="10%">
+                <a class="navbar-brand" href="index.php">Cinezona</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                    <?php
+                        if(empty($_SESSION["usuario"]) || !isset($_SESSION["usuario"])){
+                    ?>
+                    <li class="nav-item active">
+                        <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#modalLRForm" >Identifícate</button>
+                    </li>
+                    <?php
+                        }
+                        elseif(!empty($_SESSION["usuario"])){
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><?php echo "Bienvenido ".$_SESSION["usuario"];?></a>
+                    </li>
+                    <?php
+                        }
+                    ?>
+                    </ul>
+                </div>
             </div>
         </nav>
+        <div class="modal fade" id="modalLRForm" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog cascading-modal" role="document">
+                <!--Contenido-->
+                <div class="modal-content">
+
+                <!--Modaal en cascada-->
+                <div class="modal-c-tabs">
+
+                    <!-- Tabs de inicio de sesión o registro -->
+                    <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" href="#panelInicio" role="tab"><i class="fas fa-user mr-1"></i>
+                        Inicio de sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#panelRegistro" role="tab"><i class="fas fa-user-plus mr-1"></i>
+                        Registro</a>
+                    </li>
+                    </ul>
+
+                    <!--Páneles de identificación-->
+                    <div class="tab-content">
+                        <!--Panel inicio de sesión-->
+                        <div class="tab-pane fade in show active" id="panelInicio" role="tabpanel">
+
+                            <!--Cuerpo inicio de sesión-->
+                            <div class="modal-body mb-1">
+                                <form method="post">
+                                    <div class="md-form form-sm mb-5">
+                                        <label data-error="wrong" data-success="right" for="modalLRInput10">Correo</label>
+                                        <input type="email" id="correoLogin" class="form-control form-control-sm validate" name="correoLogin" placeholder="Correo">
+                                    </div>
+
+                                    <div class="md-form form-sm mb-4">
+                                        <label data-error="wrong" data-success="right" for="modalLRInput11">Contraseña</label>
+                                        <input type="password" id="passLogin" class="form-control form-control-sm validate" placeholder="Contraseña">
+                                    </div>
+                                    <div class="text-center mt-2">
+                                        <input type="submit" class="btn btn-info" value="Inicio de sesión">
+                                        <?php
+                                            if(isset($_POST["correoLogin"]))
+                                                $_SESSION["usuario"] = $_POST["correoLogin"];
+                                        ?>
+                                    </div>
+                                </form>
+                            </div>
+                            <!--Footer-->
+                            <div class="modal-footer">
+                                <!--<div class="options text-right">
+                                    <p class="pt-1">Already have an account? <a href="#" class="blue-text">Log In</a></p>
+                                </div>-->
+                                <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Cerrar</button>
+                            </div>
+
+                        </div>
+                    <!--/Panel inicio de sesión-->
+
+                    <!--Panel Registro-->
+                    <div class="tab-pane fade" id="panelRegistro" role="tabpanel">
+
+                        <!--Body-->
+                        <div class="modal-body">
+                            <form method="post">
+                                <div class="md-form form-sm mb-5">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput12">Nombre</label>
+                                    <input type="text" id="nombreRegistro" class="form-control form-control-sm validate" placeholder="Nombre">
+                                    
+                                </div>
+                                <div class="md-form form-sm mb-5">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput12">Correo</label>
+                                    <input type="email" id="emailRegistro" class="form-control form-control-sm validate" placeholder="Correo">
+                                    
+                                </div>
+
+                                <div class="md-form form-sm mb-5">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput13">Contraseña</label>
+                                    <input type="password" id="passRegistro" class="form-control form-control-sm validate" placeholder="Contraseña">
+                                    
+                                </div>
+
+                                <div class="md-form form-sm mb-4">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput14">Repite contraseña</label>
+                                    <input type="password" id="modalLRInput14" class="form-control form-control-sm validate" placeholder="Repite contraseña">
+                                </div>
+
+                                <div class="text-center form-sm mt-2">
+                                    <input type="submit" class="btn btn-info" value="Regístrate"><!--<i class="fas fa-sign-in ml-1"></i>-->
+                                </div>
+                            </form>
+                        </div>
+                        <!--Footer-->
+                        <div class="modal-footer">
+                        <!--<div class="options text-right">
+                            <p class="pt-1">Already have an account? <a href="#" class="blue-text">Log In</a></p>
+                        </div>-->
+                        <button type="button" class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                    <!--/.Panel Registro-->
+                    </div>
+
+                </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <script src="vendor\jquery\jquery.js"></script>
+        <script src="vendor\bootstrap\js\bootstrap.js"></script> 
     </body>
 </html>
--- index.php
+++ index.php
@@ -2,56 +2,174 @@
 	<body>
 		<?php
 			include "header.php";
+			ini_set("display errors", E_ALL);
+	
+			//CONEXION
+			$host="127.0.0.1";
+			$port=3306;
+			$socket="";
+			$user="root";
+			$password="Suripanta.98";
+			$dbname="cinedb";
+
+			$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+				or die ('Could not connect to the database server' . mysqli_connect_error());
+
+			//$con->close();
+
+			
+			//COMPROBAR CONEXION
+			if (mysqli_connect_errno()) {
+				printf("Conexion fallida: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			echo("Conexion exitosa");
+			
+			
+			//RECUPERAR LOS REGISTROS
+			
+			$res = mysqli_query($con, "SELECT * FROM TipoProyeccion");
+			
+			echo("<table  border=1>");
+			while ($registro = mysqli_fetch_row($res)){
+				echo("<tr><td>$registro[0]</td><td>$registro[1]</td><td>$registro[2]</td></tr>");
+			}
+			echo("</table>");
+			
+			mysqli_free_result($res);
+			mysqli_close($con);
 		?>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3">
-					<h1 class="my-4">Shop Name</h1>
-					<div class="list-group">
-					<a href="#" class="list-group-item">Category 1</a>
-					<a href="#" class="list-group-item">Category 2</a>
-					<a href="#" class="list-group-item">Category 3</a>
+		<form>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3">
+						<h1 class="my-4">Shop Name</h1>
+						<div class="list-group">
+						<a href="#" class="list-group-item">Category 1</a>
+						<a href="#" class="list-group-item">Category 2</a>
+						<a href="#" class="list-group-item">Category 3</a>
+					</div>
 				</div>
-			</div>
-	<!-- /.col-lg-3 -->
+		<!-- /.col-lg-3 -->
 
-				<div class="col-lg-9">
+					<div class="col-lg-9">
 
-					<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-						</ol>
-					<div class="carousel-inner" role="listbox">
-						<div class="carousel-item active">
-							<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+						<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+							<ol class="carousel-indicators">
+								<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+								<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+							</ol>
+						<div class="carousel-inner" role="listbox">
+							<div class="carousel-item active">
+								<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+							</div>
 						</div>
-						<div class="carousel-item">
-							<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
-						<div class="carousel-item">
-							<img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-						</div>
-					</div>
-						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
-					</div>
 
-					<div class="row">
+						<div class="row">
 
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="datosPelicula.php?idPelicula=" ><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+										<div class="card-body">
+											<h4 class="card-title">
+											<a href="#">Item One</a>
+											</h4>
+											<h5>$24.99</h5>
+											<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+										</div>
+										<div class="card-footer">
+											<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
 									<div class="card-body">
 										<h4 class="card-title">
-										<a href="#">Item One</a>
+										<a href="#">Item Two</a>
+										</h4>
+										<h5>$24.99</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+									</div>
+									<div class="card-footer">
+										<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+									<div class="card-body">
+										<h4 class="card-title">
+										<a href="#">Item Three</a>
+										</h4>
+										<h5>$24.99</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+									</div>
+									<div class="card-footer">
+										<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+									<div class="card-body">
+										<h4 class="card-title">
+										<a href="#">Item Four</a>
+										</h4>
+										<h5>$24.99</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+									</div>
+									<div class="card-footer">
+										<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+									<div class="card-body">
+										<h4 class="card-title">
+										<a href="#">Item Five</a>
+										</h4>
+										<h5>$24.99</h5>
+										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+									</div>
+									<div class="card-footer">
+										<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+									</div>
+									</div>
+								</div>
+
+								<div class="col-lg-4 col-md-6 mb-4">
+									<div class="card h-100">
+									<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+									<div class="card-body">
+										<h4 class="card-title">
+										<a href="#">Item Six</a>
 										</h4>
 										<h5>$24.99</h5>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
@@ -61,122 +179,15 @@
 									</div>
 								</div>
 							</div>
-
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-								<div class="card-body">
-									<h4 class="card-title">
-									<a href="#">Item Two</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-								</div>
-								<div class="card-footer">
-									<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-								</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-								<div class="card-body">
-									<h4 class="card-title">
-									<a href="#">Item Three</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-								</div>
-								<div class="card-footer">
-									<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-								</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-								<div class="card-body">
-									<h4 class="card-title">
-									<a href="#">Item Four</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-								</div>
-								<div class="card-footer">
-									<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-								</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-								<div class="card-body">
-									<h4 class="card-title">
-									<a href="#">Item Five</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-								</div>
-								<div class="card-footer">
-									<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-								</div>
-								</div>
-							</div>
-
-							<div class="col-lg-4 col-md-6 mb-4">
-								<div class="card h-100">
-								<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-								<div class="card-body">
-									<h4 class="card-title">
-									<a href="#">Item Six</a>
-									</h4>
-									<h5>$24.99</h5>
-									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-								</div>
-								<div class="card-footer">
-									<small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-								</div>
-							</div>
 						</div>
+					<!-- /.row -->
 					</div>
-				<!-- /.row -->
+			<!-- /.col-lg-9 -->
 				</div>
-		<!-- /.col-lg-9 -->
+		<!-- /.row -->
 			</div>
-	<!-- /.row -->
-		</div>
+		</form>
+
+		
 	</body>
 </html>
-
-<?php
-	ini_set("display errors", E_ALL);
-	
-	//CONEXION
-	$cnx = mysqli_connect("127.0.0.1:3306", "root", "", "CineDB");
-	
-	//COMPROBAR CONEXION
-	if (mysqli_connect_errno()) {
-		printf("Conexion fallida: %s\n", mysqli_connect_error());
-		exit();
-	}
-	
-	echo("Conexion exitosa");
-	
-	
-	//RECUPERAR LOS REGISTROS
-	
-	$res = mysqli_query($cnx, "SELECT * FROM TipoProyeccion");
-	
-	echo("<table  border=1>");
-	while ($registro = mysqli_fetch_row($res)){
-		echo("<tr><td>$registro[0]</td><td>$registro[1]</td><td>$registro[2]</td></tr>");
-	}
-	echo("</table>");
-
-	
-	mysqli_free_result($res);
-	mysqli_close($cnx);
-?>
